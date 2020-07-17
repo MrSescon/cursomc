63489d54c77e17a3e64b430eb931230ec5395203
Magno Sescon <magnosescon@gmail.com>
2020-07-17 08:13:11 -0300

    First Commit

---
+++ src/main/java/com/sescon/cursomc/CursomcApplication.java
@@ -0,0 +1,13 @@
+package com.sescon.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
