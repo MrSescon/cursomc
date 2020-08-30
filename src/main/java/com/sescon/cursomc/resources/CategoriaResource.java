154022bc447333f05dfa9b1a7901423518423372
Magno Sescon <magnosescon@gmail.com>
2020-08-30 13:44:34 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/sescon/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/sescon/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -37,16 +39,18 @@ public class CategoriaResource {
 	}
 
 	@PostMapping
-	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
-		obj = categoriaService.insert(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO categoriaDto) {
+		Categoria categoria = categoriaService.fromDTO(categoriaDto);
+		categoria = categoriaService.insert(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
 
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
-		obj.setId(id);
-		obj = categoriaService.update(obj);
+	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO categoriaDto, @PathVariable Integer id) {
+		Categoria categoria = categoriaService.fromDTO(categoriaDto);
+		categoria.setId(id);
+		categoria = categoriaService.update(categoria);
 		return ResponseEntity.noContent().build();
 
 	}
