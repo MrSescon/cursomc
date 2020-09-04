dc7aaf2646a9c1c6b1224e41efe00a325f9f8661
Magno Sescon <magnosescon@gmail.com>
2020-09-03 21:17:58 -0300

    PUT, DELETE e GET para Cliente

---
+++ src/main/java/com/sescon/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,38 @@
+package com.sescon.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.sescon.cursomc.domain.Cliente;
+
+import lombok.Data;
+
+@Data
+public class ClienteDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	@NotEmpty(message="Preenchimento Obrigatório")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
+	private String nome;
+	
+	@NotEmpty(message="Preenchimento Obrigatório")
+	@Email(message="Email inválido")
+	private String email;
+
+	public ClienteDTO() {
+
+	}
+
+	public ClienteDTO(Cliente cliente) {
+		id=cliente.getId();
+		nome=cliente.getNome();
+		email=cliente.getEmail();
+		
+	}
+
+}
