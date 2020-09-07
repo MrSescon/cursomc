9615ad5be44c94b80cc86b1ab0318fe4902b61f6
Magno Sescon <magnosescon@gmail.com>
2020-09-07 09:51:22 -0300

    Inserindo um novo cliente com post

--- src/main/java/com/sescon/cursomc/resources/ClienteResource.java
+++ src/main/java/com/sescon/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.sescon.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,14 +12,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sescon.cursomc.domain.Cliente;
 import com.sescon.cursomc.dto.ClienteDTO;
+import com.sescon.cursomc.dto.ClienteNewDTO;
 import com.sescon.cursomc.services.ClienteService;
 
 @RestController
@@ -33,6 +37,15 @@ public class ClienteResource {
 		Cliente obj = clienteService.find(id);
 
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO clienteNewDto) {
+		Cliente cliente = clienteService.fromDTO(clienteNewDto);
+		cliente = clienteService.insert(cliente);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(cliente.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 	@PutMapping(value = "/{id}")
