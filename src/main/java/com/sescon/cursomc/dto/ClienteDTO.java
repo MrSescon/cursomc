f8526563f36ccfa8c26fec48f0973a1a65a5ae0c
Magno Sescon <magnosescon@gmail.com>
2020-09-17 08:04:59 -0300

    Validação customizada: email não repitido na atualização do cliente

--- src/main/java/com/sescon/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/sescon/cursomc/dto/ClienteDTO.java
@@ -8,21 +8,23 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.sescon.cursomc.domain.Cliente;
+import com.sescon.cursomc.services.validation.ClienteUpdate;
 
 import lombok.Data;
 
+@ClienteUpdate
 @Data
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
-	
-	@NotEmpty(message="Preenchimento Obrigatório")
-	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
+
+	@NotEmpty(message = "Preenchimento Obrigatório")
+	@Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
-	
-	@NotEmpty(message="Preenchimento Obrigatório")
-	@Email(message="Email inválido")
+
+	@NotEmpty(message = "Preenchimento Obrigatório")
+	@Email(message = "Email inválido")
 	private String email;
 
 	public ClienteDTO() {
@@ -30,10 +32,10 @@ public class ClienteDTO implements Serializable {
 	}
 
 	public ClienteDTO(Cliente cliente) {
-		id=cliente.getId();
-		nome=cliente.getNome();
-		email=cliente.getEmail();
-		
+		id = cliente.getId();
+		nome = cliente.getNome();
+		email = cliente.getEmail();
+
 	}
 
 }
